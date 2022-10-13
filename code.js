275b3132ebf130964b46d1243c4c8b1fc4740436
Wyatt DeRamus <deramus.wyatt@gmail.com>
2022-10-13 10:28:32 -0500

    unit testing

--- code.js
+++ code.js
@@ -2,3 +2,12 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function subtract(x, y) {
+    if (typeof x === "number" && typeof y === "number") {
+        return x - y;
+    }else {
+        return false;
+
+    }
+}
--- tests.js
+++ tests.js
@@ -13,3 +13,32 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe("subtract", function(){
+    it("should be a defined function", function(){
+       expect(typeof subtract).toBe ("function");
+    });
+    it("should return a number when passed two number arguments", function(){
+        expect(typeof subtract(3,1)).toBe("number");
+    });
+    it("should return false when passed any arguement that is not a number", function (){
+        expect(subtract()).toBe(false);
+        expect(subtract(2)).toBe(false);
+        expect(subtract("Banana", 6)).toBe(false);
+    });
+    it("should return 34 when passed the arguements of 64 and 30", function (){
+        expect(subtract(64, 30)).toBe(34);
+    });
+    it("should return 34 when passed the arguements of 100 and 25", function (){
+        expect(subtract(100, 25)).toBe(75);
+    });
+    it("should return 34 when passed the arguements of 0 and 7", function (){
+        expect(subtract(0, 7)).toBe(-7);
+    });
+    it("should return 34 when passed the arguements of 310 and 10", function (){
+        expect(subtract(310, 10)).toBe(300);
+    });
+    it("should return 34 when passed the arguements of 10 and 0", function (){
+        expect(subtract(10, 0)).toBe(10);
+    });
+});
